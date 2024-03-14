f3aa6f591ff9704c959aa0a4ce246e640721b125
Dante Samael Fernández Lago <dante2402006@hybridge.education>
2024-03-14 14:18:50 -0500

    Edit .py files.

--- The Legend of Python/gif_based_on_text/client.py
+++ The Legend of Python/gif_based_on_text/client.py
@@ -1,27 +1,34 @@
-import random, discord
+import os, random, discord
 import get_gif as gg
-
-api_key = '' # Replace with your own API key.
 
 def choose_word_from_words(words):
     return random.choice(words)
-
 class Client(discord.Client):
     async def on_ready(self):
         print(f'You are successfully logged in as {self.user}')
-    
+
     async def on_message(self, message):
         if message.author == self.user:
             return
 
-        if message.content != '':
-            chosen_word = choose_word_from_words(message.content.split())
-            gifs = gg.get(chosen_word)
-            chosen_gif = random.choice(gifs['data'])['images']
-            await message.channel.send(chosen_gif['original']['url'])
+        if message.content:
+            await self.send_gif(message)
+
+    async def send_gif(self, message):
+        chosen_word = choose_word_from_words(message.content.split())
+        gifs = gg.get(chosen_word)
+        
+        if not gifs:
+            print(f"No gifs found for word: {chosen_word}")
+            return
+        
+        chosen_gif = random.choice(gifs['data'])['images']
+        await message.channel.send(chosen_gif['original']['url'])    
+
+discord_bot_api_key = os.getenv('DISCORD_BOT_API_KEY')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run(api_key)
+client.run(discord_bot_api_key)
--- The Legend of Python/gif_based_on_text/get_gif.py
+++ The Legend of Python/gif_based_on_text/get_gif.py
@@ -1,8 +1,8 @@
-import json, requests
+import os, json, requests
 
-api_key = '' # Replace with your own API key.
+giphy_api_key = os.getenv('GIPHY_API_KEY')
 
 def get(search):
-    response = requests.get(f'https://api.giphy.com/v1/gifs/search?api_key={api_key}&q={search}&limit=5')
+    response = requests.get(f'https://api.giphy.com/v1/gifs/search?api_key={giphy_api_key}&q={search}&limit=5')
     json_data = json.loads(response.text)
     return json_data
